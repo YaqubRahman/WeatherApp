79efb180ec347f1fe99e14a38b42c7a325902222
YaqubRahman <YaqubRahman999@gmail.com>
2024-12-19 16:00:28 +0000

    Can now input weather and it can output the details

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 function getWeather(){
     const apiKey = 'b73c20664d11109d96fb5fc980e3927b';
-    
     const city = document.getElementById('city').value;
 
     if (!city){
         alert('Please enter a city')
         return;
     }
+
     const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    const forcastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid${apiKey}`;
+    const forcastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
     
     fetch(currentWeatherUrl)
         .then(response => response.json())
@@ -22,10 +22,19 @@ function getWeather(){
          
 }
 
+function displayWeather(data){
+    const displayDiv = document.getElementById('weather-display')
+    displayDiv.innerHTML = ` 
+        <h2>Weather in ${data.name}</h2>
+        <p>Temperature: ${(data.main.temp - 273.15).toFixed(2)}°C</p>
+        <p>Weather: ${data.weather[0].description}</p>
+        <p>Humidity: ${data.main.humidity}%</p>
+    `;
+}
 
 
 
-fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13')
-    .then(res => res.json())
-    .then(data => console.log(data))
+//fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13')
+//    .then(res => res.json())
+//    .then(data => console.log(data))
 
